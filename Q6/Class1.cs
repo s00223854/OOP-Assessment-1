64580f80fac6b144aa728c064c422d71eefd2afa
eoinkilgallonITS <eoinkilgallon@gmail.com>
2022-10-17 11:12:43 +0100

    Q6 finished

---
+++ Q6/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public Decimal HourlyRate { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Worker Name: {Name}");
+            Console.WriteLine($"Worker Type: {Type}");
+            Console.WriteLine($"Worker Hourly Rate: {HourlyRate}");
+        }
+    }
+  
+}
--- Q6/Program.cs
+++ Q6/Program.cs
@@ -10,6 +10,18 @@ namespace Q6
     {
         static void Main(string[] args)
         {
+            Worker worker1 = new Worker();
+            worker1.Name = "Tom";
+            worker1.Type = "Plumber";
+            worker1.HourlyRate = 15;
+            Worker worker2 = new Worker();
+            worker2.Name = "Jo";
+            worker2.Type = "Electrician";
+            worker2.HourlyRate = 20;
+
+            worker1.DisplayInfo();
+            worker2.DisplayInfo();
+
         }
     }
 }
