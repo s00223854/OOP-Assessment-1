1276ec1f23ff87cce279742ac47d19be9a01db89
eoinkilgallonITS <eoinkilgallon@gmail.com>
2022-10-17 10:42:26 +0100

    Q3 started

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -10,6 +10,35 @@ namespace Q2
     {
         static void Main(string[] args)
         {
+            //Q2
+            string userPlace;
+            int discount;
+            int fee = 100;
+
+
+            Console.WriteLine("Enter your place of residence");
+            userPlace = Convert.ToString(Console.ReadLine());
+
+            
+
+            if (userPlace == "sligo")
+            {
+                discount = 30;
+            }
+            if (userPlace == "ireland")
+            {
+                discount = 20;
+            }
+            if (userPlace == "europe")
+            {
+                discount = 10;
+            }
+            else
+                discount = 0;
+
+            Console.WriteLine("Your fee is: ",discount - fee);
+
         }
+
     }
 }
